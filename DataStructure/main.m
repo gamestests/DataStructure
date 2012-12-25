9e9755880f624ee5ee7185914d6e7a4bf33e2693
gamestests <gamestests@sina.com>
2012-12-25 13:47:54 -0800

    Initial Commit

---
+++ DataStructure/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DataStructure
+//
+//  Created by gxs on 12-12-25.
+//  Copyright (c) 2012年 gxs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
